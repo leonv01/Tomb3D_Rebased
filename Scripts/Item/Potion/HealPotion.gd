c67b6796d5862ab50ea7b9179cc8d17b4168b9a8
Leon <leonvogt2001@gmail.com>
2024-08-31 22:00:12 +0200

    - Working chest system - Items transferable between player inventory and chest - Stacked items splitable with rightclick in inventory - Cigarette instance

---
+++ Scripts/Item/Potion/HealPotion.gd
@@ -0,0 +1,13 @@
+extends Potion
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	super()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	super(delta)
+	pass
